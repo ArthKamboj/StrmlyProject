3e089d7b666985b43de1b3a0135782ff39c9494a
Arth Kamboj <arth23153015@akgec.ac.in>
2025-06-21 23:54:48 +0530

    Used helmet for API protection

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const helmet = require('helmet')
 const cors = require('cors')
 const authRoutes = require('./routes/auth')
 const videoRoutes = require('./routes/video')
 
 const app = express();
 
+app.use(helmet())
 app.use(cors())
 app.use(express.json())
 app.use('/api/auth', authRoutes)
